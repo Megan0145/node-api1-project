f80e5b22fb45c0bf55e53632e4a0cc7c2f385958
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 12:25:03 +0000

    Scaffold API, deps imported, default request handler function implemented

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+    res.json('boo')
+}
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('listening on ' + (process.env.PORT || 5000))
+})
