89550d72e072f4c9b95ef9adfa0c8fc969154301
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 21:47:20 +0000

    Refactor delete fucntion to parse payload to int

--- users/src/state/reducers.js
+++ users/src/state/reducers.js
@@ -1,16 +1,21 @@
-import * as types from './actionTypes';
+import * as types from "./actionTypes";
 
 const initState = {
-    users: []
-}
+  users: []
+};
 
-export function userReducer(state = initState, action){
-    switch(action.type){
-        case types.GET_USERS: 
-            return {users: action.payload}
-            case types.DELETE_USER:
-                return {...state.users, users: state.users.filter(user => {return user.id != action.payload})}
-        default: 
-        return state;
-    }
+export function userReducer(state = initState, action) {
+  switch (action.type) {
+    case types.GET_USERS:
+      return { users: action.payload };
+    case types.DELETE_USER:
+      return {
+        ...state.users,
+        users: state.users.filter(user => {
+          return user.id !== JSON.parse(action.payload);
+        })
+      };
+    default:
+      return state;
+  }
 }
