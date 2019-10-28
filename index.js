be93901c6e9e7e6a7ad447574bd725beb1930bd8
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 13:10:44 +0000

    Endpoint to get user by id

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
 // implement your API here
-const express = require("express")
-const cors = require("cors")
-const db = require("./data/db")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const db = require("./data/db");
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.post("/api/users", createNewUser)
-app.get("/api/users", getAllUsers)
-app.get("*", handleDefaultRequest)
+app.get("/api/users/:id", getUserById);
+app.post("/api/users", createNewUser);
+app.get("/api/users", getAllUsers);
+app.get("*", handleDefaultRequest);
+
+function getUserById(req, res) {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+}
 
 function createNewUser(req, res) {
-    const user = {
-        name: req.body.name,
-        bio: req.body.bio
-    }
-    if(!user.name || !user.bio){
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
-    } else {
-        db.insert(user)
-        .then(response => {
-            res.status(201).json(user)
-        })
-        .catch(err => {
-            res.status(500).json({ error: "There was an error while saving the user to the database" })
-        })
-    }
+  const user = {
+    name: req.body.name,
+    bio: req.body.bio
+  };
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert(user)
+      .then(response => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the user to the database"
+          });
+      });
+  }
 }
 
 function getAllUsers(req, res) {
   db.find()
     .then(data => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch(err => {
-       res.status(500).json({ error: "The users information could not be retrieved." })
-    })
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
 }
 
 function handleDefaultRequest(req, res) {
@@ -45,4 +73,4 @@ function handleDefaultRequest(req, res) {
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("listening on " + (process.env.PORT || 5000));
-})
+});
