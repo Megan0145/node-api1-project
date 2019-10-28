a7fa9245b09ab1159dd7059be557d32f3e796fc7
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 12:33:15 +0000

    Endpoint to get all users

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 // implement your API here
-const express = require('express')
-const cors = require('cors')
-
+const express = require("express")
+const cors = require("cors")
+const db = require("./data/db")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('*', handleDefaultRequest)
+app.get("/api/users", getAllUsers)
+app.get("*", handleDefaultRequest)
+
+function getAllUsers(req, res) {
+  db.find()
+    .then(data => {
+      console.log(data)
+      res.json(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
 
 function handleDefaultRequest(req, res) {
-    res.json('boo')
+  res.json("boo");
 }
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log('listening on ' + (process.env.PORT || 5000))
+  console.log("listening on " + (process.env.PORT || 5000));
 })
