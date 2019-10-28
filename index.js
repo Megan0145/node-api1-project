a351268fd1702144d8c97e92b56718349e7d4ae9
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 12:55:40 +0000

    Create user endpoint

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.post("/api/users", createNewUser)
 app.get("/api/users", getAllUsers)
 app.get("*", handleDefaultRequest)
+
+function createNewUser(req, res) {
+    const user = {
+        name: req.body.name,
+        bio: req.body.bio
+    }
+    if(!user.name || !user.bio){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.insert(user)
+        .then(response => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+    }
+}
 
 function getAllUsers(req, res) {
   db.find()
