f49964713c1b21cfeab5bbbd86b4fbb0007af22b
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-28 13:37:34 +0000

    Update endpoint

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.put("/api/users/:id", updateUser)
 app.delete("/api/users/:id", deleteUser)
 app.get("/api/users/:id", getUserById);
 app.post("/api/users", createNewUser);
@@ -26,6 +27,24 @@ function deleteUser(req, res) {
     .catch(err => {
         res.status(500).json({ error: "The user could not be removed" })
     })
+}
+
+function updateUser(req, res) {
+const { id } = req.params;
+const { name, bio } = req.body;
+
+    if(!name || !bio){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.update(id, req.body)
+        .then(user => {
+            console.log(id, req.body)
+            res.status(200).json({message: "User updated", body: req.body})
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be modified." })
+        })
+    }
 }
 
 function getUserById(req, res) {
